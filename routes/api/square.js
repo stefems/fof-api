0caa979997f92441fd94dcf4acb2f2dc8062990a
Stefan Kowal <kowalmax.s@gmail.com>
2021-07-23 01:09:12 -0400

    ugh

--- routes/api/square.js
+++ routes/api/square.js
@@ -27,7 +27,7 @@ router.post("/purchase", async (req, res, next) => {
 	} catch (error) {
 		// todo
 		console.log(error)
-		res.status(500).send({ success: false, error: error })
+		res.status(500).send({ error: error })
 	}
 });
 
